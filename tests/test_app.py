25c483afd589893678da512491a37d65c80e6bee
Tom Freed <tom.freed@outlook.com>
2023-09-13 15:07:08 +0100

    TF: amending test file

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,9 +2,10 @@ import pytest
 from flask import url_for, session
 from flask_testing import TestCase
 from application import app, db
-from models import User, Screen, Movie, Screening, Discussion, Booking
+from models import User, Screen, Movie, Screening, Discussion, Booking, BookingDetail
 import os
 from werkzeug.security import generate_password_hash
+
 
 class TestBase(TestCase):
     def create_app(self):
@@ -39,8 +40,8 @@ class TestBase(TestCase):
         db.session.commit()
 
         test_movies =  [
-            Movie(title="Test Movie", director="Test Director", actors="Actor1, Actor2, Actor3", release_date="2023-01-01", description="A test movie description", poster="movie_poster.jpg", classic=True, age_restricted=False),
-            Movie(title="Test Movie2", director="Test Director", actors="Actor1, Actor2, Actor3", release_date="2023-01-01", description="A test movie description", poster="movie_poster2.jpg", classic=False, age_restricted=True)
+            Movie(title="Test_Movie(classic)", director="Test Director", actors="Actor1, Actor2, Actor3", release_date="2023-01-01", description="A test movie description", poster="movie_poster.jpg", classic=True, age_restricted=False),
+            Movie(title="Test_Movie(new release)", director="Test Director", actors="Actor1, Actor2, Actor3", release_date="2023-01-01", description="A test movie description", poster="movie_poster2.jpg", classic=False, age_restricted=True)
         ]
         db.session.add_all(test_movies)
         db.session.commit()
@@ -66,26 +67,6 @@ class TestBase(TestCase):
         ]
         db.session.add_all(test_discussions)
         db.session.commit()
-
-        # booking_test = Booking(
-        #         user_id=1,
-        #         screening_id=5,
-        #         booking_date="2023-01-01 10:00:00",
-        #         total_price=50,
-        #         discounted_ticket_number=2,
-        #         full_price_ticket_number=3
-        #     )
-        # db.session.add(booking_test)
-        # db.session.commit()
-
-        # booking_details_test = BookingDetail(
-        #         booking_id=2,
-        #         ticket_type="Adult",
-        #         quantity=2,
-        #         price=50
-        #     )
-        # db.session.add(booking_details_test)
-        # db.session.commit()
 
     def tearDown(self):
         session.clear()
@@ -126,39 +107,54 @@ class TestStaticPages(TestBase):
     def test_view_movie_get(self):
         response = self.client.get(url_for("view_movie", movie_id=1))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Test Movie", response.data)
+        self.assertIn(b"Test_Movie(classic)", response.data)
 
+    def test_listings_get(self):
+        response = self.client.get(url_for("listings"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Test_Movie(classic)", response.data)
+        self.assertIn(b"Test_Movie(new release)", response.data)
 
+    def test_new_releases_get(self):
+        response = self.client.get(url_for("new_releases"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Test_Movie(new release)", response.data)
+        self.assertNotIn(b"Test_Movie(classic)", response.data)
 
+    def test_classics_get(self):
+        response = self.client.get(url_for("classics"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Test_Movie(classic)", response.data)
+        self.assertNotIn(b"Test_Movie(new release)", response.data)
+
+    def test_search_results_get(self):
+        response = self.client.get(url_for("search_results"))
+        self.assertEqual(response.status_code, 200)
+
+class TestAPI(TestBase):
     def test_api_view_screenings_get(self):
         response = self.client.get(
             url_for("api_view_screenings", movie_id=1),
             query_string={"day": "Friday"}
             )
         self.assertEqual(response.status_code, 200)
-        
+        self.assertIn(b"12:00:00", response.data)
+        #self.assertIn(b"Test_Movie(classic)", response.data)
+        #check with Alex re 'title' in screening data
 
-    def test_listings_get(self):
-        response = self.client.get(url_for("listings"))
+    def test_api_view_screenings_no_screenings(self):
+        response = self.client.get(
+            url_for("api_view_screenings", movie_id=3),
+            query_string={"day": "Friday"}
+            )
         self.assertEqual(response.status_code, 200)
-
-    def test_new_releases_get(self):
-        response = self.client.get(url_for("new_releases"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_classics_get(self):
-        response = self.client.get(url_for("classics"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_search_results_get(self):
-        response = self.client.get(url_for("search_results"))
-        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"There are currently no showings of this film", response.data)
 
 class TestPayment(TestBase):
     def test_payment_get(self):
         response = self.client.get(url_for("payment", screening_id=1))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Test Movie", response.data)
+        self.assertIn(b"Test_Movie(classic)", response.data)
         self.assertIn(b"Friday", response.data)
         self.assertIn(b"12:00:00", response.data)
     
@@ -178,8 +174,6 @@ class TestPayment(TestBase):
             }
         )
         self.assertEqual(response.status_code, 200)
-
-
 
 class TestSignup(TestBase):
     def test_signup_get(self):
@@ -314,7 +308,7 @@ class TestForum(TestBase):
         response = self.client.get(url_for("forum"))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Test Topic 1", response.data)
-        self.assertIn(b"Test Movie", response.data)
+        self.assertIn(b"Test_Movie(classic)", response.data)
         self.assertIn(b"testuser", response.data)
 
     def test_post_to_forum(self):
@@ -371,11 +365,40 @@ class TestForum(TestBase):
         self.assertNotIn(b"shit", response.data)
 
 
+class TestBooking(TestBase):
+    def test_booking_get(self):
+        response = self.client.get(url_for("book_movie", screening_id=1))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Test_Movie(classic)", response.data)
+        self.assertIn(b"12:00:00", response.data)
+        self.assertIn(b"Friday", response.data)
 
-    # def test_booking_get(self):
-    #     # Replace <screening_id> with an actual screening_id to test the route
-    #     response = self.client.get(url_for("book_movie", screening_id=<screening_id>))
-    #     self.assertEqual(response.status_code, 200)
+    def test_booking_post(self):
+        with self.client.session_transaction() as session:
+            session["user_id"] = 1
+        response = self.client.post(
+            url_for("book_movie", screening_id=1),
+        data={
+            "Adult":1,
+            "Child":1,
+            "Concession":1
+            },
+        follow_redirects = True
+        )
+        # check Booking database
+        check_booking = Booking.query.filter_by(user_id=1).first()
+        self.assertIsNotNone(check_booking)
+        self.assertEqual(check_booking.total_price, 32.5)
+        # check BookingDetail database
+        check_booking_detail = BookingDetail.query.filter_by(booking_id=1).all()
+        self.assertIsNotNone(check_booking_detail)
+        self.assertEqual(check_booking_detail[0].ticket_type, "Adult")
+        self.assertEqual(check_booking_detail[1].ticket_type, "Child")
+        self.assertEqual(check_booking_detail[2].ticket_type, "Concession")
+        # check redirect to Payment
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Payment Form", response.data)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
